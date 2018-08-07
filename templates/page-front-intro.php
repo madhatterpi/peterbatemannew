6eba8521025f0ce9fac748f79d359754a87522de
madhatterpi <peter_bateman@hotmail.co.uk>
2018-08-07 12:45:48 +0100

    big changes about

---
+++ templates/page-front-intro.php
@@ -0,0 +1,22 @@
+<section id="welcome" class="section">
+  <div class="intro-block__outer">
+    <div class="intro-block intro-block-small">
+    <div class="intro-block__inner">
+      <div class="intro-box">
+        <span class="intro-box__text">Affordable websites for start-ups and small businesses. All built with the latest technologies and coding practices</span>
+      </div>
+    </div>
+  </div>
+  <div class="intro-block intro-block--large">
+    <div class="intro-block__inner">
+      <div class="intro-about">
+            <h2 class="intro-about__header">I'm a freelance web developer specialising in Wordpress</h2>
+        <p class="intro-about__text">I love getting to know the companies I work with and working closely together to create something amazing. My attention to detail and personal approach sets me apart from the rest. </p>
+        <a href="<?php echo get_site_url()?>/about" class="button button--border">about me</a>
+      </div>
+
+    </div>
+  </div>
+  </div>
+
+</section>
---
+++ templates/page-services.php
@@ -0,0 +1,54 @@
+<section class="container-fluid services-section section">
+  <div class="services-section__row">
+      <h2 class="header services-section__header">what I do</h2>
+      <p class="services-section__intro">Whether you need a completely new site along with a new logo or an overhaul of an e-commerce site, I have the skill set to help you produce it.</p>
+    </div>
+
+    <div class="services-section__row skills">
+      <div class="skills__column">
+        <div class="skills__icon show">
+          <i class="far fa-desktop" aria-hidden="true"></i>
+        </div>
+        <h2 class="header skills__header">web design</h2>
+        <p class="skills__text">I make websites that balance great design and easy to use interfaces. I use adobe illustrator and photoshop for mockups.</p>
+      </div>
+      <div class="skills__column">
+        <div class="skills__icon show">
+          <i class="far fa-cogs" aria-hidden="true"></i>
+        </div>
+        <h2 class="header skills__header">development</h2>
+        <p class="skills__text">I build fast, responsive and future-proof websites. I mostly use Wordpress but also use Squarespace when more of a budget is present.</p>
+      </div>
+      <div class="skills__column">
+        <div class="skills__icon show">
+          <i class="far fa-search" aria-hidden="true"></i>
+        </div>
+        <h2 class="header skills__header">SEO</h2>
+        <p class="skills__text">The goal posts are constantly being moved for how a website gets ranked in search results, I keep track of these changes and implement a number of tactics to keep you on top of competitors.</p>
+      </div>
+    </div>
+
+    <div class="services-section__row skills">
+      <div class="skills__column">
+        <div class="skills__icon show-b">
+          <i class="fab fa-wordpress" aria-hidden="true"></i>
+        </div>
+        <h2 class="header skills__header">wordpress</h2>
+        <p class="skills__text">Wordpress is the most used content management system in the world and with it I create highly functional websites that has an easy to understand and use backend.</p>
+      </div>
+      <div class="skills__column">
+        <div class="skills__icon show-b">
+          <i class="far fa-shopping-cart" aria-hidden="true"></i>
+        </div>
+        <h2 class="header skills__header">e-commerce</h2>
+        <p class="skills__text">I use WooCommerce for e-commerce websites, it's an incredibly capable plugin with lots of features and best of all - it's free.</p>
+      </div>
+      <div class="skills__column">
+        <div class="skills__icon show-b">
+          <i class="far fa-database" aria-hidden="true"></i>
+        </div>
+        <h2 class="header skills__header">hosting</h2>
+        <p class="skills__text">Getting a good and reliable host is vital. I offer a modern hosting package which works for you and helps me maintain the quality and up-time of your site.</p>
+      </div>
+    </div>
+</section>
